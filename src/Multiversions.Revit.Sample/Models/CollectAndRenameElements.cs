410de7fe2bfb169ca732ac52837cfe64bedf2b7d
Mahesh-Peddinti <maheshpeddinti331504@gmail.com>
2026-07-13 18:09:03 +0530

    updated

---
+++ src/Multiversions.Revit.Sample/Models/CollectAndRenameElements.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+
+namespace Multiversions.Revit.Sample.Models
+{
+    public class CollectAndRenameElements : IExternalEventHandler
+    {
+        public void Execute(UIApplication app)
+        {
+            Autodesk.Revit.DB.Document doc = app.ActiveUIDocument.Document;
+            Collection<Reference> walls = new FilteredElementCollector(doc)
+                .OfClass(typeof(Wall))
+                .Cast<Wall>() as Collection<Reference>;
+
+        }
+
+        public string GetName()
+        {
+            return "Collect and Rename Elements";
+        }
+    }
+}
